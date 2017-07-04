dfeaac4c4f3e1bcfb986765e15a1bf9fce69dd2f
Ashutosh Kumar <ashu.nitc009@gmail.com>
2017-07-04 22:25:44 +0530

    Update meadowlark.js

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
-var handlebars = require('express3-handlebars').create({defaultLayout:'Main'});
-var fortunes = require('./lib/fortune.js')
+// var handlebars = require('express3-handlebars').create({defaultLayout:'Main'});
+// var fortunes = require('./lib/fortune.js')
 
 app.set('port',process.env.PORT || 3000)
 app.engine('handlebars',handlebars.engine);
@@ -9,19 +9,19 @@ app.set('view engine','handlebars')
 
 
 app.get("/",(req , res)=>{
-    // res.type('text/plain');
-    // res.status('200');
-    // res.send("meadowlark home page");
+    res.type('text/plain');
+    res.status('200');
+    res.send("meadowlark home page");
     res.render('home');
 })
 
-app.get("/about",(req , res)=>{
-    // res.type('text/plain');
-    // res.status('200');
-    // res.send("meadowlark about page");
-    //var randomFortune = fortunes[Math.floor( Math.random() * fortunes.length)];
-    res.render('about',{fortune : fortune.getFortune()});
-})
+// app.get("/about",(req , res)=>{
+//     // res.type('text/plain');
+//     // res.status('200');
+//     // res.send("meadowlark about page");
+//     //var randomFortune = fortunes[Math.floor( Math.random() * fortunes.length)];
+//     res.render('about',{fortune : fortune.getFortune()});
+// })
 
 app.use(express.static(__dirname + '/public'));
 app.use(function(req,res){
